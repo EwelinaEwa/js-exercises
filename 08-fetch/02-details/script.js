c011fa67180b2bbadb12c0a217dea358726aaff2
EwelinaEwa <ewelina.kosciukiewicz@gmail.com>
2021-06-23 17:02:18 +0200

    Complete chapter 8, ex.2

--- 08-fetch/02-details/script.js
+++ 08-fetch/02-details/script.js
@@ -9,4 +9,30 @@
 
 (() => {
     // your code here
+
+
+
+        fetch('http://localhost:63342/js-exercises/_shared/api.json.')
+            .then((response) => {
+                return response.json()
+            })
+            .then((data) => {
+                document.getElementById("run").addEventListener("click", function () {
+                    let input = document.getElementById("hero-id").value - 1;
+                    let hero = data.heroes[input];
+
+                    let heroTemplate = document.getElementById("tpl-hero").content;
+                    let copyTemplate = document.importNode(heroTemplate,true);
+
+                    copyTemplate.querySelector(".name").innerHTML = hero.name;
+                    copyTemplate.querySelector(".alter-ego").innerHTML = hero.alterEgo;
+                    copyTemplate.querySelector(".powers").innerHTML = hero.abilities;
+
+                    document.getElementById("target").appendChild(copyTemplate);
+
+                })
+            })
+
+
+
 })();
