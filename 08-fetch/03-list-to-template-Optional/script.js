1a9a38994b02ad940f749de73c6ec41375de5cd4
EwelinaEwa <ewelina.kosciukiewicz@gmail.com>
2021-06-24 10:06:42 +0200

    Complete chapter 8, ex.3 - WIP

--- 08-fetch/03-list-to-template-Optional/script.js
+++ 08-fetch/03-list-to-template-Optional/script.js
@@ -9,4 +9,42 @@
 
 (() => {
     // your code here
+    fetch('http://localhost:63342/js-exercises/_shared/api.json.')
+        .then((response) => {
+            return response.json();
+        })
+        .then((data) => {
+            document.getElementById("run").addEventListener("click", function () {
+                let heroes = data.heroes;
+                // console.log(heroes)
+
+                let heroTemplate = document.getElementById("tpl-hero").content;
+                let copyTemplate = document.importNode(heroTemplate, true);
+
+                // heroes.forEach((hero, index) => {
+                //     copyTemplate.querySelector(".hero").innerHTML = index;
+                //     copyTemplate.querySelector(".name").innerHTML = hero.name;
+                //     copyTemplate.querySelector(".alter-ego").innerHTML = hero.alterEgo;
+                //     copyTemplate.querySelector(".powers").innerHTML = hero.abilities;
+                //
+                //     document.getElementById("target").appendChild(copyTemplate);
+                //
+                // })
+
+
+                for (let i=0; i<heroes.length; i++) {
+                    let hero = data.heroes[i]
+                    console.log(hero)
+                    copyTemplate.querySelector(".name").innerHTML = heroes[i].name;
+                    copyTemplate.querySelector(".alter-ego").innerHTML = heroes[i].alterEgo;
+                    copyTemplate.querySelector(".powers").innerHTML = heroes[i].abilities;
+
+                    document.getElementById("target").appendChild(copyTemplate);
+
+                }
+
+            })
+
+        })
+
 })();
