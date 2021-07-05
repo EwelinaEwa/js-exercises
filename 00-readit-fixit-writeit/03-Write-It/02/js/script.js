7b5a6f00ce52cb4faa7814c16bf2d2b74c49a05b
EwelinaEwa <ewelina.kosciukiewicz@gmail.com>
2021-07-05 12:32:17 +0200

    Complete chapter 0, 03-write-it, 03 (& 02 redone)

--- 00-readit-fixit-writeit/03-Write-It/02/js/script.js
+++ 00-readit-fixit-writeit/03-Write-It/02/js/script.js
@@ -14,17 +14,28 @@ const movies = [
     `Bon Bini: Judeska in da House`,
 ];
 
-const wrapWithTag = (content, tagname) => {
-    return `<${tagname}>${content}</${tagname}>`;
-};
-const parseTitle = title => {
-    const lowerTitle = title.toLowerCase();
-    const wrapped = wrapWithTag(lowerTitle, `li`);
-    return wrapped;
-};
+// const wrapWithTag = (content, tagname) => {
+//     return `<${tagname}>${content}</${tagname}>`;
+// };
+// const parseTitle = title => {
+//     const lowerTitle = title.toLowerCase();
+//     const wrapped = wrapWithTag(lowerTitle, `li`);
+//     return wrapped;
+// };
+//
+// document.write(`<ol>`);
+// for (let i = movies.length-1; i>=0; i--) {
+//     document.write(parseTitle(movies[i]));
+// }
+// document.write(`</ol>`);
 
-document.write(`<ol>`);
-for (let i = movies.length-1; i>=0; i--) {
-    document.write(parseTitle(movies[i]));
+const wrapWithTag = (content, tagname) => {return `<${tagname}>${content}</${tagname}>`}
+
+document.write(`<ol>`)
+for (let i=movies.length-1; i>=0; i--) {
+   document.write(wrapWithTag(movies[i].toLowerCase(), `li`))
 }
-document.write(`</ol>`);
+document.write(`</ol>`)
+
+
+
---
+++ 00-readit-fixit-writeit/03-Write-It/03/script.js
@@ -0,0 +1,62 @@
+const winners = [
+    {
+    actor: "Forest Whitaker",
+    film: "The Last King of Scotland",
+    age: 45
+    }, {
+    actor: "Daniel Day-Lewis",
+    film: "There Will Be Blood",
+    age: 50
+}, {
+    actor: "Sean Penn",
+    film: "Milk",
+    age: 48
+}, {
+    actor: "Jeff Bridges",
+    film: "Crazy Heart",
+    age: 60
+}, {
+    actor: "Colin Firth",
+    film: "The King's Speech",
+    age: 50
+}, {
+    actor: "Jean Dujardin",
+    film: "The Artist",
+    age: 39
+}, {
+    actor: "Daniel Day-Lewis",
+    film: "Lincoln",
+    age: 55
+}, {
+    actor: "Matthew McConaughey",
+    film: "Dallas Buyers Club",
+    age: 44
+}, {
+    actor: "Eddie Redmayne",
+    film: "The Theory of Everything",
+    age: 33
+}, {
+    actor: "Leonardo DiCaprio",
+    film: "The Revenant",
+    age: 41
+}
+]
+let wrappedActor = (actor, tag) => {return `<${tag}>${actor}</${tag}>`}
+
+winners.sort((a,b) => {
+    return a.age - b.age;
+});
+
+document.write(`<ol>`)
+winners.forEach((winner, index) => {
+    let winningActor = (index, winner.actor)
+    document.write(wrappedActor(winningActor, `li`))
+});
+document.write(`</ol>`)
+
+console.log(winners.reduce((a,b) => ({age: a.age + b.age})));
+
+
+
+
+
